a23b584e3bf564155b423e4442de45000224bff8
Moses Mugoya <mugoya.moses@students.mak.ac.ug>
2025-07-07 12:20:09 +0300

    Implement custom user model and update settings; modify Docker Compose command

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,47 @@
+"""
+Database models
+"""
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    '''Custom manager for user model that uses email as the unique identifier.'''
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and return a user with an email and password."""
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and return a superuser with an email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class user(AbstractUser,PermissionsMixin):
+    """Custom user model that supports email instead of username."""
+    email = models.EmailField( max_length=225,unique=True)
+    name = models.CharField(max_length=225)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
+    def __str__(self):
+        return self.email
---
+++ app/core/test/test_models.py
@@ -0,0 +1,19 @@
+"""
+Tests for models
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+    """Test models."""
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful."""
+        email = 'test@example.com'
+        password = 'testpass123'
+        User = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(User.email, email)
+        self.assertTrue(User.check_password(password))
+
