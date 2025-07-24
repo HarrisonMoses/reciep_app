d0d1ac6a0704de3ed3eb99cfb862333d1f2914f7
Moses Mugoya <mugoya.moses@students.mak.ac.ug>
2025-07-24 09:52:29 +0300

    Fix import statement for DjangoFilterBackend in views.py

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -10,7 +10,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.authentication import TokenAuthentication
-from rest_framework import DjangoFilterBackend
+from django_filters.rest_framework import DjangoFilterBackend
 
 from core.models import (Recipe, Tag, Ingredient)
 from recipe.serializers import( RecipeSerializer,
